0d0315280850ab59e526576c71640a4eaa05b5ac
charles-cg <carlitoscarreong@hotmail.com>
2026-01-28 00:23:22 -0600

    completed exercise 09 of JS foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+  if (!Number.isInteger(n1) || !Number.isInteger(n2)) {
+    return "ERROR";
+  } else if (n1 < 0 || n2 < 0) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+
+  if (n1 < n2) {
+    for (let i = n1; i <= n2; i++) {
+      sum += i;
+    }
+    return sum;
+  } else if (n1 > n2) {
+    for (let j = n2; j <= n1; j++) {
+      sum += j;
+    }
+    return sum;
+  } else {
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
