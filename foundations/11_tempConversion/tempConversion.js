61d67e8fd8a6b6239a2d882925c3665c9fcd654c
charles-cg <carlitoscarreong@hotmail.com>
2026-01-28 15:44:14 -0600

    completed exercise 11 of JS foundation

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) * (5 / 9);
+
+  let roundCelsius = Math.round(celsius * 10) / 10;
+
+  return roundCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = (temp * (9 / 5) + 32);
+
+  let roundFahrenheit = Math.round(fahrenheit * 10) / 10;
+
+  return roundFahrenheit;
 };
 
 // Do not edit below this line
