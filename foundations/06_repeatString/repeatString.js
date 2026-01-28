9a867a1fce113e5c7a640c929a589f115671ba0b
charles-cg <carlitoscarreong@hotmail.com>
2026-01-27 20:30:41 -0600

    completed exercise 06 of JS foundations

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, numRepeats) {
+  let repeated = "";
+  if (numRepeats < 0) return "ERROR";
 
+  for (let i = 0; i < numRepeats; i++) {
+    repeated += str;
+  }
+
+  return repeated;
 };
 
 // Do not edit below this line
