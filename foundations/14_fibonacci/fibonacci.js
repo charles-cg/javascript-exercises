ce280d390589e26a1213e6edaa5c567a47207547
charles-cg <carlitoscarreong@hotmail.com>
2026-02-06 13:59:39 -0600

    completed exercise 14 of foundations

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+  let count;
+
+  if (typeof member !== 'number') {
+    count = parseInt(member);
+  } else {
+    count = member;
+  }
+
+  if (count < 0) {
+    return "OOPS";
+  }
+  if (count == 0) {
+    return 0;
+  }
+
+  let fibo = [1, 1];
+
+  if (count == 1 || count == 2) {
+    return 1;
+  } else {
+    for (let i = 0; i < (member - 2); i++) {
+      fibo.push(fibo[fibo.length - 1] + fibo[fibo.length - 2]);
+    }
+
+    return fibo[fibo.length - 1];
+  }
+
 
 };
 
