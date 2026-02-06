c4ce910c779fad2799f22ce8a4bced8e0ca65b6a
charles-cg <carlitoscarreong@hotmail.com>
2026-02-06 13:37:08 -0600

    completed exercise 13 of foundations

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  let cleanWord = word
+  .toLowerCase()
+  .split('')
+  .filter(character => characters.includes(character))
+  .join('')
+
+  for (let i = 0; i < cleanWord.length; i++) {
+    if (cleanWord[i] !== cleanWord[cleanWord.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 
 };
 
