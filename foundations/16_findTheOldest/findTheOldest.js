e6d9ac1e213d693a49afe7995671fa3a4a673010
charles-cg <carlitoscarreong@hotmail.com>
2026-02-09 14:47:42 -0600

    completed exercise 16 of JS foundation

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldest, currentPerson) => {
+    let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
 
+    if (oldestAge < currentAge) {
+      return currentPerson;
+    } else {
+      return oldest;
+    }
+
+  })
 };
+
+const getAge = function(birthYear, deathYear) {
+  const currentYear = new Date().getFullYear();
+
+  if (deathYear == undefined) {
+    let age = currentYear - birthYear;
+    return age;
+  } else {
+    let age = deathYear - birthYear;
+    return age;
+  }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
